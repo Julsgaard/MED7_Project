542ac15a66016c5defd8ff543f79aba62e4a7b4a
Julsgaard <100912975+Julsgaard@users.noreply.github.com>
2024-11-24 13:16:47 +0100

    Settings button with connect to server
    Can be used if the client is disconnected

--- MED7_Unity/Assets/Scripts/NetworkManagerUI.cs
+++ MED7_Unity/Assets/Scripts/NetworkManagerUI.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,19 +5,20 @@ using UnityEngine.UI;
 public class NetworkManagerUI : MonoBehaviour
 {
     [SerializeField] private Button clientButton;
-    [SerializeField] private Button hostButton;
+    //[SerializeField] private Button hostButton;
     [SerializeField] private Button serverButton;
-
+    
     private void Awake()
     {
+        // Add listeners to the buttons
         serverButton.onClick.AddListener(() =>
         {
             NetworkManager.Singleton.StartServer();
         });
-        hostButton.onClick.AddListener(() =>
+        /*hostButton.onClick.AddListener(() =>
         {
             NetworkManager.Singleton.StartHost();
-        });
+        });*/
         clientButton.onClick.AddListener(() =>
         {
             NetworkManager.Singleton.StartClient();
