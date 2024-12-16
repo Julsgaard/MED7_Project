7f538353b2d02d38618fe091daadf31820c9b267
Julsgaard <100912975+Julsgaard@users.noreply.github.com>
2024-12-16 22:40:57 +0100

    Updated text

--- MED7_Unity/Assets/scripts/RotatePostItText.cs
+++ MED7_Unity/Assets/scripts/RotatePostItText.cs
@@ -14,22 +14,23 @@ public class RotatePostItText : MonoBehaviour
 
     private IEnumerator RotateTextTowardsPlayer()
     {
+        // Infinite loop until the coroutine is stopped
         while (true)
         {
+            // Calculate the direction from the text to the player
             Vector3 newDir = mainCamera.transform.position - transform.position;
-            newDir.y = 0;
-            // Debug.Log(newDir);
+            newDir.y = 0; // Ignore the y-axis
 
+            // Calculate the angle between the text and the player
             float angle = Mathf.Atan2(newDir.x, newDir.z) * Mathf.Rad2Deg;
             
-            // clamp angle to nearest 90 deg
+            // Clamp angle to nearest 90 deg
             float clampedAngle = (float)Math.Round(((angle + 180) / 360) * 4) * 90;
-            // Debug.Log($"Angle: {angle}. ClampedAngle: {clampedAngle}");
             
+            // Rotate the text towards the player
             transform.localRotation = Quaternion.Euler(90, 0, -clampedAngle);
             
-            // controls update frequency
-            yield return new WaitForSeconds(0.5f);
+            yield return new WaitForSeconds(0.5f); // Update every 0.5 seconds
         }
     }
 
